bf651c9c032fa9cf3a5eacbd3ff053376224f70a
Alexander <alez.grante@gmail.com>
2022-04-10 22:11:12 +0300

    scan works

--- app/src/main/java/com/example/materialdesignapp/FirstFragment.kt
+++ app/src/main/java/com/example/materialdesignapp/FirstFragment.kt
@@ -1,22 +1,28 @@
 package com.example.materialdesignapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.materialdesignapp.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
 import com.example.materialdesignapp.databinding.FragmentFirstBinding
+import io.github.g00fy2.quickie.QRResult
+import io.github.g00fy2.quickie.ScanQRCode
 
 class FirstFragment : Fragment() {
 
     private lateinit var binding: FragmentFirstBinding
+    private val scanQrCode = registerForActivityResult(ScanQRCode(), ::handleResult)
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        binding = FragmentFirstBinding.inflate(layoutInflater,container,false)
+        binding = FragmentFirstBinding.inflate(layoutInflater, container, false)
+
 
         binding.btn.setOnClickListener {
             parentFragmentManager.beginTransaction()
@@ -26,6 +32,16 @@ class FirstFragment : Fragment() {
                 .commit()
         }
 
+        binding.fabAddProduct.setOnClickListener {
+            scanQrCode.launch(null)
+        }
+
         return binding.root
     }
+
+    private fun handleResult(result: QRResult) {
+        Log.d("loger",(result as QRResult.QRSuccess).content.rawValue)
+    }
+
+
 }
