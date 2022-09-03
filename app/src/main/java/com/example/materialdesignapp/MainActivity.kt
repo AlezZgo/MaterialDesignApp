281d6e6060f0db37d72af1f6e14130821c2c4d5a
Александр <alez.grante@gmail.com>
2022-09-04 00:21:58 +0300

    redesign

--- app/src/main/java/com/example/materialdesignapp/MainActivity.kt
+++ app/src/main/java/com/example/materialdesignapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.materialdesignapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.materialdesignapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,16 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        setContentView(binding.root)
-        val navView: BottomNavigationView = binding.bottomNavigationView
-        val navController = findNavController(R.id.nav_host_fragment)
-
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment_activity_main
+        ) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView = binding.navView
+        navView.background = null
         navView.setupWithNavController(navController)
-
+        setContentView(binding.root)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
